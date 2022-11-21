e82e7d21454236ef1a476b34a9d0e10e63aad87c
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-11-22 01:05:38 +0700

    done to auth feature

--- src/service/api.js
+++ src/service/api.js
@@ -1,79 +1,55 @@
 import axios from 'axios'
 
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000/api/',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        }
+  })
 
-
-
+const JWT_TOKEN_LOCALSTORAGE_KEY = 'jwt_token'
 
 
 export const authAPI = {
     async login(email, password) {
-        const response = await fetch("http://localhost:8000/api/login/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
-        }).then(response => response.json().then(data => ({
-            status: response.status,
-            body: data
-        })));
-
-        return response;
-
+        console.log(email,password)
+        const json = JSON.stringify({ "email": email, "password": password });
+        const response = await axiosInstance.post('login/', json)
+        if (response.status == 200) {
+            localStorage.setItem(JWT_TOKEN_LOCALSTORAGE_KEY, response.data.jwt)
+            return true
+        }
+        return false
+    },
+    async auth(){
+        const token = localStorage.getItem(JWT_TOKEN_LOCALSTORAGE_KEY)
+        const json = JSON.stringify({ "token":token });
+        const response = await axiosInstance.post('user/',json)
+        if (response.status == 200) {
+            return response.data
+        }
+        return {}
+    },
+    async register(name,email, password,image) {
+        const json = JSON.stringify({ "name":name,"email": email, "password": password ,"user_profile":[{
+            "imageProfile":image
+        }]});
+        const response = await axiosInstance.post('register/', json)
+        if (response.status == 200) {
+            return true
+        }
+        return false
 
 
 
     },
-    async register(email, password, username, picture) {
-        const response = await fetch("http://localhost:8000/api/register/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                username: username,
-                image: picture,
-            }),
-        }).then(response => response.json().then(data => ({
-            status: response.status,
-            body: data
-        })));
-
-        return response;
-    },
-    async authen() {
-        const response = await fetch("http://localhost:8000/api/user/", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        }).then(response => response.json().then(data => ({
-            status: response.status,
-            body: data
-        })));
-
-        return response;
-    },
-    async logout() {
-        const response = await fetch("http://localhost:8000/api/logout/", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            credentials: "include",
-
-        }).then
-            (response => response.json().then(data => ({ status: response.status, body: data })));
-
-        return response;
+    logout(){
+        localStorage.removeItem(JWT_TOKEN_LOCALSTORAGE_KEY)
     }
 
+
+   
 
 }
 
