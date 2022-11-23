35acfa2fb9898d63b55e23c41a0adac2554b8e0d
SaluteKevin <salute.k@ku.th>
2022-11-23 10:15:10 +0700

    update 23/11/2565

--- src/components/Haircard.vue
+++ src/components/Haircard.vue
@@ -3,18 +3,23 @@
   <div class="card my-3 " style="width:28rem;">
 
     <div class="row mt-3">
-        <div class="col"><img class="profile-border " src="../assets/default.png"></div>
-          <div class="col text-start">
+        <div class="col-3"><img class="profile-border floated-end previewImg" src="../assets/default.png"></div>
+          <div class="col-6 text-start">
             <label>{{title}}</label>
             <br>
             <label>{{username}}</label>
         </div>
-    </div>
-
-    <div class="card-body px-3">
-      <router-link to="/ShowHairStyle"></router-link>
-      <img alt="Card image cap" class="card-img-top" src="../assets/hairOne.png">
-      <p class="card-text text-start mt-2">{{subtitle}}</p>
+        <div class="col">
+          <a class="mt-auto btn btn-primary justify-content-end" href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+          </svg></a>
+        </div>
+    
+      
+      <div class="mt-3">
+        <router-link to="/ShowHairStyle"></router-link>
+        <img type="button" @click="$router.push('ShowHairStyle')" alt="card image cap" class="card-img-bottom" src="../assets/hairOne.png">
+      </div>
     </div>
   </div>
   
@@ -45,6 +50,12 @@ export default {
   border-right-color: #2c2b2b;
   width: 50px;
   height: 50px;
+}
+
+.previewImg{
+  margin-left: auto;
+  margin-right: auto;
+  object-fit: cover;
 }
 
 </style>
